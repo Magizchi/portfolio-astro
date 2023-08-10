93c3b8b48211a660568fa931aab4b5a7a0539fc2
Rajanan <g.rajan1993@gmail.com>
2023-08-10 19:19:47 +0200

    fix url

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: ' https://magizchi.github.io/portfolio_astro/',
+  site: 'https://magizchi.github.io/portfolio_astro/',
   integrations: [mdx(), sitemap(), tailwind({})],
 });
