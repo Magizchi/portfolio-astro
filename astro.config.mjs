8263047f26e28fa6edc7d420ea0527901132e23f
Rajanan <g.rajan1993@gmail.com>
2023-08-10 18:29:31 +0200

    tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,5 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
-  integrations: [mdx(), sitemap(), tailwind({ applyBaseStyles: false })],
+  integrations: [mdx(), sitemap(), tailwind({})],
 });
