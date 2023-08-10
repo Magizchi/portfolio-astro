9963681b37f09484a9be100635c7f10962aae485
Rajanan <g.rajan1993@gmail.com>
2023-08-10 19:17:40 +0200

    add site

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
+  site: ' https://magizchi.github.io/portfolio_astro/',
   integrations: [mdx(), sitemap(), tailwind({})],
 });
