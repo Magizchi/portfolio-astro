2a23c98f2ff5a3ab06ef2fbdb2b6fe7778bdc49d
Rajanan <g.rajan1993@gmail.com>
2023-08-11 19:08:16 +0200

    change ts confg

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,13 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "hero-pattern": "url('/public/repeat-unsplash.jpg')",
-      },
-    },
-  },
-  plugins: [
-  ],
+  theme: {},
+  plugins: [],
 };
