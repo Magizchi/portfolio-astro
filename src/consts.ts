fc560dbdc6ac7d4764de91cffb6b1554359cbabb
Rajanan <g.rajan1993@gmail.com>
2023-10-26 12:36:36 +0200

    feat(meta-info): add meta info for each page

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,11 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
-
+// HOME PAGE INFO
 export const SITE_TITLE = 'rajanan.dev';
 export const SITE_DESCRIPTION = 'Welcome to my website!';
+// EXPERIENCE PAGE INFO
+export const SITE_TITLE_EXPERIENCE = 'rajanan.dev | Mes expériences';
+export const SITE_DESCRIPTION_EXPERIENCE = 'Ici vous pouvez visualiser mes expériences en tant de développeur Front et me connaitre d\'avantage'
+// PROJECT PAGE INFO
+export const SITE_TITLE_PROJECTS = 'rajanan.dev | Éxpositions';
+export const SITE_PROJECTS_DESCRIPTION = 'Ici vous pouvez visualiser les projets que j\'ai développés avec plusieurs techo diffèrent'
