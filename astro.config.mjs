4f086021501a1837de162d3f3e5101cfb1529534
Rajanan <g.rajan1993@gmail.com>
2024-02-05 18:53:15 +0100

    add react and path for import

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,15 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://rajanan.dev',
-  integrations: [mdx(), sitemap(), tailwind({})],
+  integrations: [mdx(), sitemap(), tailwind({}), react({
+    include: ['**/react/*'],
+    experimentalReactChildren: true,
+  })],
   redirects: {
     '/': '/experiences'
   }
