bba7049a7a7570078ace4976027f15df3eae3fda
Rajanan <g.rajan1993@gmail.com>
2024-10-31 15:58:39 +0100

    refato+add good import

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,9 @@
+const colors = require('tailwindcss/colors')
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
-  theme: {},
+  content: ["./src/**/*.{astro, ts}"],
+  theme: {
+  },
   plugins: [],
+  darkMode:["class"],
 };
