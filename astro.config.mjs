57604dd12744acc3252ee5119809d66a1c93fc9f
Rajanan <g.rajan1993@gmail.com>
2024-12-27 17:59:35 +0100

    config astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,11 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-import tailwind from "@astrojs/tailwind";
-import react from "@astrojs/react";
-import icon from "astro-icon";
+import tailwind from '@astrojs/tailwind';
+import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://rajanan.dev',
-  integrations: [mdx(), sitemap(), tailwind({}), react({
-    include: ['**/react/*'],
-    experimentalReactChildren: true,
-  }), icon()],
+	site: 'https://rajanan.dev',
+	integrations: [mdx(), sitemap(), tailwind({}), icon()]
 });
