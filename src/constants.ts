86d818b8255424d8f3d8ef476b5a1405bc3f27aa
Rajanan <g.rajan1993@gmail.com>
2024-12-27 17:59:24 +0100

    const add

---
+++ src/constants.ts
@@ -0,0 +1,14 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+// HOME PAGE INFO
+export const SITE_TITLE = 'rajanan.dev';
+export const SITE_DESCRIPTION = 'Welcome to my website!';
+// EXPERIENCE PAGE INFO
+export const SITE_TITLE_EXPERIENCE = 'rajanan.dev | Mes expériences';
+export const SITE_DESCRIPTION_EXPERIENCE = 'Ici vous pouvez visualiser mes expériences en tant de développeur Front et me connaître d\'avantage';
+// PROJECT PAGE INFO
+export const SITE_TITLE_PROJECTS = 'rajanan.dev | Expositions';
+export const SITE_PROJECTS_DESCRIPTION = 'Ici vous pouvez visualiser les projets que j\'ai développés avec plusieurs techno diffèrent';
+
+export const link_LinkedIn = "https://www.linkedin.com/in/rajanan-ganesalingam/";
+export const link_Github = "https://github.com/Magizchi";
